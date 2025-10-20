4fc9fc90028acf00df0aa0ee403a200bc0aa1d8b
rajeshvohra <37680402+rajeshvohra@users.noreply.github.com>
2025-10-20 12:29:26 -0700

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "azure_subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-}
-
-variable "azure_tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-}
